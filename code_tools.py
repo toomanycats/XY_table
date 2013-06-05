677699c717aea3a87d57fd21c7b76e53766285b3
toomanycats <dpcuneo@gmail.com>
2013-06-05 13:48:45 -0700

    Added SI unit converter used in motor_tools.py

--- code_tools.py
+++ code_tools.py
@@ -1,3 +1,4 @@
+import math
 import smtplib
 from email.mime.text import MIMEText
 
@@ -31,4 +32,36 @@ class CodeTools(object):
         s.sendmail(me, recipients, msg.as_string())
         s.quit()
 
+    def ToSI(self,d):
+        if d == 0.0 or d == 0:
+            return 0
 
+        incPrefixes = ['k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y']
+        decPrefixes = ['m', 'mu', 'n', 'p', 'f', 'a', 'z', 'y']
+
+        degree = int(math.floor(math.log10(math.fabs(d)) / 3))
+
+        prefix = ''
+
+        if degree!=0:
+            ds = degree/math.fabs(degree)
+            if ds == 1:
+                if degree - 1 < len(incPrefixes):
+                    prefix = incPrefixes[degree - 1]
+                else:
+                    prefix = incPrefixes[-1]
+                    degree = len(incPrefixes)
+            elif ds == -1:
+                if -degree - 1 < len(decPrefixes):
+                    prefix = decPrefixes[-degree - 1]
+                else:
+                    prefix = decPrefixes[-1]
+                    degree = -len(decPrefixes)
+
+            scaled = float(d * math.pow(1000, -degree))
+            s = "{scaled} {prefix}".format(scaled=scaled, prefix=prefix)
+
+        else:
+            s = "{d}".format(d=d)
+
+        return(s)
