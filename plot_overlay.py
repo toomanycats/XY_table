82f6356df54e20fc9b63baebdfcb2327ced8c555
toomanycats <dpcuneo@gmail.com>
2013-08-11 16:07:47 -0700

    Made the lattice overlay color white.

--- plot_overlay.py
+++ plot_overlay.py
@@ -5,7 +5,6 @@ import copy
 import numpy as np
 import matplotlib.pyplot as plt
 plt.ion()
-thresh = 1
 
 config = code_tools.ConfigureDataSet()
 config.load_config('/home/daniel/Downloads/test.cfg')
@@ -23,9 +22,12 @@ lattice = plt.imread('/home/daniel/Downloads/epsilon.png')
 im = plt.imshow(real[0,:,:],vmin=v_min,vmax=v_max,extent=(0,32,0,32),interpolation='bilinear',cmap='jet')
 
 lattice_mod = lattice
-lattice_mod[lattice_mod > 0] = np.nan # insert 'bad' values into your lattice (the white)
+#lattice_mod[lattice_mod < 1 ] = 0
 
-im2 = plt.imshow(lattice_mod,extent=(0,32,0,32),cmap=my_cmap)
+lattice_mod[lattice_mod > 0] = np.nan # insert 'bad' values into your lattice (the white)
+lattice_mod[lattice_mod == 0] = 255
+ 
+im2 = plt.imshow(lattice_mod,extent=(0,32,0,32),cmap=my_cmap,vmin=0, vmax=255)
 
 plt.show()
 
