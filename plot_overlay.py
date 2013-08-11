5ea4c74607c7cafc0f4cffae56fb98bb9421f825
toomanycats <dpcuneo@gmail.com>
2013-08-11 11:27:33 -0700

    Basic changes...

--- plot_overlay.py
+++ plot_overlay.py
@@ -19,11 +19,13 @@ real, v_min, v_max = plottools.get_data_type_scale(data,'real')
 my_cmap = copy.copy(plt.cm.get_cmap('gray')) # get a copy of the gray color map
 my_cmap.set_bad(alpha=0) # set how the colormap handles 'bad' values
 
-lattice = plt.imread('path')
-im = plt.imshow(data[0,:,:],vmin=v_min,vmax=v_max,extent=(0,32,0,32),interpolation='nearest',cmap='jet')
+lattice = plt.imread('/home/daniel/Downloads/epsilon.png')
+im = plt.imshow(real[0,:,:],vmin=v_min,vmax=v_max,extent=(0,32,0,32),interpolation='bilinear',cmap='jet')
 
-lattice[lattice < thresh] = np.nan # insert 'bad' values into your lattice (the white)
+lattice_mod = lattice
+lattice_mod[lattice_mod > 0] = np.nan # insert 'bad' values into your lattice (the white)
 
-im2 = plt.imshow(lattice,extent=(0,32,0,32),cmap=my_cmap)
+im2 = plt.imshow(lattice_mod,extent=(0,32,0,32),cmap=my_cmap)
+
 plt.show()
 
