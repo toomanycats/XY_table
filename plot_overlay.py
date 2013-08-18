76f0b8d18cb9df736a995226d23b381079e8e6ab
toomanycats <dpcuneo@gmail.com>
2013-08-17 17:55:12 -0700

    Experimenting with how to use the API to make and modify plot/figure objects.

--- plot_overlay.py
+++ plot_overlay.py
@@ -12,8 +12,10 @@ config.load_config('/home/daniel/Downloads/test.cfg')
 arraytools = code_tools.ArrayTools(config)
 plottools = code_tools.PlotTools(config)
 
-data = arraytools.load_mat('/home/daniel/Downloads/test.mat')
-real, v_min, v_max = plottools.get_data_type_scale(data,'real')
+data_dict = arraytools.load_mat('/home/daniel/Downloads/test.mat')
+real = data_dict['comp'].real
+inten = data_dict['inten']
+v_min, v_max = plottools.get_data_scale(data_dict,'real')
 
 my_cmap = copy.copy(plt.cm.get_cmap('gray')) # get a copy of the gray color map
 my_cmap.set_bad(alpha=0) # set how the colormap handles 'bad' values
@@ -31,3 +33,9 @@ im2 = plt.imshow(lattice_mod,extent=(0,32,0,32),cmap=my_cmap,vmin=0, vmax=255)
 
 plt.show()
 
+###
+#print "\n*********testing*********\n"
+#im = plt.imshow(inten[0,:,:])
+#im.set_extent((0,32,0,32))
+#fig = im.get_figure()
+#fig.canvas.draw()
